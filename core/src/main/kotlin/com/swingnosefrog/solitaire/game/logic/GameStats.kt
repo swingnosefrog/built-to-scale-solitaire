8ec3dab384ae320793085ae09dbb267fc990df37
chrislo27 <kinchrislo@gmail.com>
2025-06-28 13:08:42 -0700

    Add working moves counter

--- core/src/main/kotlin/com/swingnosefrog/solitaire/game/logic/GameStats.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/game/logic/GameStats.kt
@@ -3,15 +3,30 @@ package com.swingnosefrog.solitaire.game.logic
 import paintbox.binding.FloatVar
 import paintbox.binding.IntVar
 import paintbox.binding.ReadOnlyFloatVar
+import paintbox.binding.ReadOnlyIntVar
 
 
 class GameStats(private val logic: GameLogic) {
-    
+
     private val _timeElapsedSec: FloatVar = FloatVar(0f)
     val timeElapsedSec: ReadOnlyFloatVar get() = _timeElapsedSec
-    
-    val movesMade: IntVar = IntVar(0)
-    
+
+    private val _movesMade: IntVar = IntVar(0)
+    val movesMade: ReadOnlyIntVar get() = _movesMade
+
+    init {
+        logic.eventDispatcher.addListener(object : GameEventListener.Adapter() {
+
+            override fun onCardStackPlacedDown(
+                gameLogic: GameLogic,
+                cardStack: CardStack,
+                toZone: CardZone,
+            ) {
+                _movesMade.incrementAndGet()
+            }
+        })
+    }
+
     fun renderUpdate(deltaSec: Float) {
         if (!logic.isStillDealing.get()) {
             _timeElapsedSec.set(_timeElapsedSec.get() + deltaSec)
