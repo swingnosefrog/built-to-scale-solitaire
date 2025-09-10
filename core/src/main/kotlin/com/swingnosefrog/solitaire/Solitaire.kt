0c8db5d3234e20816cdf6b40efe33f74353b9603
chrislo27 <kinchrislo@gmail.com>
2025-09-10 16:44:33 -0700

    Update plugin version

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -14,7 +14,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 1, 0, "dev_20250713b")
+    val VERSION: Version = Version(0, 1, 0, "dev_20250910a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
