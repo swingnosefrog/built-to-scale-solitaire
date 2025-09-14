e2e1ff436242573faf0f25555292c5d15ba966f2
chrislo27 <kinchrislo@gmail.com>
2025-09-13 19:35:25 -0700

    Bump version

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -14,7 +14,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 1, 0, "dev_20250910a")
+    val VERSION: Version = Version(0, 1, 0, "dev_20250913a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
