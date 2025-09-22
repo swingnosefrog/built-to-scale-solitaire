ad0fb50dcb21fbe84eb79f59d1c5e320d960ed51
chrislo27 <kinchrislo@gmail.com>
2025-09-22 06:48:01 -0700

    Split out FourPane class

--- core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/MainGameHowToPlayPane.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/MainGameHowToPlayPane.kt
@@ -3,6 +3,7 @@ package com.swingnosefrog.solitaire.screen.main
 import com.badlogic.gdx.graphics.Color
 import com.swingnosefrog.solitaire.fonts.SolitaireFonts
 import com.swingnosefrog.solitaire.game.GameContainer
+import com.swingnosefrog.solitaire.ui.FourPane
 import paintbox.binding.ReadOnlyVar
 import paintbox.font.Markup
 import paintbox.ui.Anchor
@@ -31,7 +32,7 @@ class MainGameHowToPlayPane(
                 this.padding.set(Insets(16f))
                 this.bindHeightToParent(adjust = -(48f + 24f))
 
-                this += FourPane().apply {
+                this += FourPane(Color.WHITE, 2f).apply {
                 }
             }
 
@@ -49,49 +50,5 @@ class MainGameHowToPlayPane(
 
     override fun onClosePressed() {
         uiInputHandler.closeHowToPlayMenu()
-    }
-
-    private class FourPane : Pane() {
-        
-        val corners: Map<Corner, Pane>
-        
-        init {
-            val borderColor = Color(1f, 1f, 1f, 1f)
-            val borderWidth = 2f
-            this += RectElement(borderColor).apply { 
-                this.bounds.width.set(borderWidth)
-                Anchor.TopCentre.configure(this, offsetX = -(borderWidth / 2))
-            }
-            this += RectElement(borderColor).apply { 
-                this.bounds.height.set(borderWidth)
-                Anchor.CentreLeft.configure(this, offsetY = -(borderWidth / 2))
-            }
-            
-            fun createCornerPane(): Pane {
-                return Pane().apply { 
-                    this.bindWidthToParent(multiplier = 0.5f, adjust = -(borderWidth * 4))
-                    this.bindHeightToParent(multiplier = 0.5f, adjust = -(borderWidth * 4))
-                }
-            }
-            
-            corners = mapOf(
-                Corner.TOP_LEFT to createCornerPane().apply { 
-                    Anchor.TopLeft.configure(this)
-                },
-                Corner.TOP_RIGHT to createCornerPane().apply { 
-                    Anchor.TopRight.configure(this)
-                },
-                Corner.BOTTOM_LEFT to createCornerPane().apply { 
-                    Anchor.BottomLeft.configure(this)
-                },
-                Corner.BOTTOM_RIGHT to createCornerPane().apply { 
-                    Anchor.BottomRight.configure(this)
-                },
-            )
-            
-            corners.values.forEach { p -> this.addChild(p) }
-        }
-        
-        operator fun get(corner: Corner): Pane = this.corners.getValue(corner)
     }
 }
---
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/ui/FourPane.kt
@@ -0,0 +1,54 @@
+package com.swingnosefrog.solitaire.ui
+
+import com.badlogic.gdx.graphics.Color
+import paintbox.ui.Anchor
+import paintbox.ui.Corner
+import paintbox.ui.Pane
+import paintbox.ui.element.RectElement
+import kotlin.collections.forEach
+
+
+open class FourPane(
+    borderColor: Color = Color(1f, 1f, 1f, 1f),
+    borderWidth: Float = 2f,
+) : Pane() {
+
+    val corners: Map<Corner, Pane>
+
+    init {
+        this += RectElement(borderColor).apply {
+            this.bounds.width.set(borderWidth)
+            Anchor.TopCentre.configure(this, offsetX = -(borderWidth / 2))
+        }
+        this += RectElement(borderColor).apply {
+            this.bounds.height.set(borderWidth)
+            Anchor.CentreLeft.configure(this, offsetY = -(borderWidth / 2))
+        }
+
+        fun createCornerPane(): Pane {
+            return Pane().apply {
+                this.bindWidthToParent(multiplier = 0.5f, adjust = -(borderWidth * 4))
+                this.bindHeightToParent(multiplier = 0.5f, adjust = -(borderWidth * 4))
+            }
+        }
+
+        corners = mapOf(
+            Corner.TOP_LEFT to createCornerPane().apply {
+                Anchor.TopLeft.configure(this)
+            },
+            Corner.TOP_RIGHT to createCornerPane().apply {
+                Anchor.TopRight.configure(this)
+            },
+            Corner.BOTTOM_LEFT to createCornerPane().apply {
+                Anchor.BottomLeft.configure(this)
+            },
+            Corner.BOTTOM_RIGHT to createCornerPane().apply {
+                Anchor.BottomRight.configure(this)
+            },
+        )
+
+        corners.values.forEach { p -> this.addChild(p) }
+    }
+
+    operator fun get(corner: Corner): Pane = this.corners.getValue(corner)
+}
