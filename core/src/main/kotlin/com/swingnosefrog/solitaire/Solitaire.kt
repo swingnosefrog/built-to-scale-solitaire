73ad87a476d691745d1ced40977e47aa257ca896
chrislo27 <kinchrislo@gmail.com>
2025-09-28 20:46:41 -0700

    Update to Kotlin 2.2.20

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -15,7 +15,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 3, 0, "dev_20250922a")
+    val VERSION: Version = Version(0, 3, 0, "dev_20250929a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
