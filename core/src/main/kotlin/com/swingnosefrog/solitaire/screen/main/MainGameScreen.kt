6d3462c6a90e32414030181400b8a7f0dcda7565
chrislo27 <kinchrislo@gmail.com>
2025-10-19 13:27:45 -0700

    Refactor MainGameScreen to reduce state clutter

--- core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/MainGameScreen.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/MainGameScreen.kt
@@ -2,12 +2,15 @@ package com.swingnosefrog.solitaire.screen.main
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.Disposable
 import com.badlogic.gdx.utils.viewport.ScalingViewport
 import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.swingnosefrog.solitaire.Solitaire
 import com.swingnosefrog.solitaire.screen.AbstractGameScreen
 import com.swingnosefrog.solitaire.SolitaireGame
 import com.swingnosefrog.solitaire.game.GameContainer
@@ -30,44 +33,22 @@ class MainGameScreen(
     }
 
     private val batch: SpriteBatch = main.batch
+    
     private val screenInputMultiplexer: InputMultiplexer = InputMultiplexer()
+    private val toggleableGameInputProcessor: ToggleableInputProcessor
 
     private val soundSystem: SoundSystem = SoundSystem.createDefaultSoundSystem()
     private val gameMusic: GameMusic = GameMusic(soundSystem)
 
-    private val gameInputMultiplexer: InputMultiplexer = InputMultiplexer()
-    private var _backingGameContainer: GameContainer? = null
-    private var backingGameContainer: GameContainer
-        set(newValue) {
-            val oldValue = _backingGameContainer
-            if (oldValue != null) {
-                this.gameInputMultiplexer.removeProcessor(oldValue.inputProcessor)
-            }
-
-            newValue.resize(Gdx.graphics.width, Gdx.graphics.height)
-            newValue.gameRenderer.shouldApplyViewport.set(true)
-            this.gameInputMultiplexer.addProcessor(newValue.inputProcessor)
-            _backingGameContainer = newValue
-            (gameContainer as Var).set(newValue)
-
-            oldValue?.dispose()
-        }
-        get() = _backingGameContainer ?: error("backingGameContainer is not initialized yet")
-    val gameContainer: ReadOnlyVar<GameContainer> by lazy { Var(backingGameContainer) }
-
-    // Note: Game framebuffer is set to stretch scaling, so initial width/height doesn't matter and can be 1
-    private val gameFrameBufferCamera: OrthographicCamera = OrthographicCamera().apply { 
-        setToOrtho(false, 1f, 1f)
-    }
-    private val gameFrameBufferViewport: ScalingViewport = StretchViewport(1f, 1f, gameFrameBufferCamera)
-    private val gameFrameBufferMgr: FrameBufferManager = FrameBufferManager(
-        1, FrameBufferManager.BufferSettings(Pixmap.Format.RGB888), scaling = gameFrameBufferViewport.scaling,
-    )
+    private val backingGameContainer: BackingGameContainer = BackingGameContainer()
+    val gameContainer: ReadOnlyVar<GameContainer> get() = backingGameContainer.currentContainer
 
     private val ui: MainGameUi = MainGameUi(this)
-    private val toggleableGameInputProcessor: ToggleableInputProcessor = ToggleableInputProcessor(gameInputMultiplexer)
-            
+    
+    private val gameFrameBuffer: GameFrameBuffer = GameFrameBuffer()
+
     init {
+        toggleableGameInputProcessor = ToggleableInputProcessor(backingGameContainer)
         toggleableGameInputProcessor.enabled.bind { ui.currentMenuState.use() == MainGameUi.MenuState.NONE }
         
         this.screenInputMultiplexer.addProcessor(ui.inputProcessor)
@@ -76,21 +57,23 @@ class MainGameScreen(
         
         soundSystem.startRealtime()
         
-        this.screenInputMultiplexer.addProcessor(DebugInputAdapter(this, this.ui))
+        if (Solitaire.isNonProductionVersion) {
+            this.screenInputMultiplexer.addProcessor(DebugInputAdapter(this, this.ui))
+        }
         this.screenInputMultiplexer.addProcessor(toggleableGameInputProcessor)
     }
     
     fun startNewGame(deckInitializer: DeckInitializer) {
         val newContainer = GameContainer({ GameLogic(deckInitializer) }, batch, soundSystem, gameMusic)
-        backingGameContainer = newContainer
+        backingGameContainer.setNewGameContainer(newContainer)
         
         gameMusic.transitionToStemMix(GameMusic.StemMixes.ALL, 1f)
     }
 
     override fun render(delta: Float) {
         super.render(delta)
-        
-        gameFrameBufferMgr.frameUpdate()
+
+        gameFrameBuffer.fbManager.frameUpdate()
 
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
@@ -106,13 +89,13 @@ class MainGameScreen(
     }
 
     private fun renderGameToBuffer(delta: Float) {
-        gameFrameBufferMgr.getFramebuffer(GAME_BUFFER_INDEX)?.also { gameFb ->
+        gameFrameBuffer.fbManager.getFramebuffer(GAME_BUFFER_INDEX)?.also { gameFb ->
             gameFb.begin()
 
             Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-            val gameRenderer = backingGameContainer.gameRenderer
+            val gameRenderer = gameContainer.getOrCompute().gameRenderer
             gameRenderer.render(delta)
 
             gameFb.end()
@@ -121,13 +104,13 @@ class MainGameScreen(
 
     private fun renderGameBufferToBackBuffer() {
         val batch = batch
-        val cam = gameFrameBufferCamera
-        gameFrameBufferViewport.apply()
+        val cam = gameFrameBuffer.camera
+        gameFrameBuffer.viewport.apply()
 
         batch.projectionMatrix = cam.combined
         batch.begin()
 
-        val gameFbRegion = gameFrameBufferMgr.getFramebufferRegion(GAME_BUFFER_INDEX)
+        val gameFbRegion = gameFrameBuffer.fbManager.getFramebufferRegion(GAME_BUFFER_INDEX)
         batch.setColor(1f, 1f, 1f, 1f)
         batch.draw(gameFbRegion, 0f, 0f, cam.viewportWidth, cam.viewportHeight)
 
@@ -137,15 +120,15 @@ class MainGameScreen(
     override fun renderUpdate() {
         super.renderUpdate()
 
-        val gameLogic = backingGameContainer.gameLogic
+        val gameLogic = gameContainer.getOrCompute().gameLogic
         gameLogic.renderUpdate(Gdx.graphics.deltaTime)
     }
 
     override fun resize(width: Int, height: Int) {
         super.resize(width, height)
-        gameFrameBufferViewport.update(width, height)
+        gameFrameBuffer.viewport.update(width, height)
         ui.resize(width, height)
-        backingGameContainer.resize(width, height)
+        gameContainer.getOrCompute().resize(width, height)
     }
     
     override fun show() {
@@ -172,6 +155,57 @@ ${ui.getDebugString()}
         gameMusic.dispose()
         soundSystem.dispose()
         
-        gameFrameBufferMgr.dispose()
+        gameFrameBuffer.dispose()
+    }
+    
+    private class BackingGameContainer private constructor(
+        private val gameInputMultiplexer: InputMultiplexer,
+    ) : Disposable, InputProcessor by gameInputMultiplexer {
+        
+        private var _container: GameContainer? = null
+        private var current: GameContainer
+            set(newValue) {
+                val oldValue = _container
+                if (oldValue != null) {
+                    this.gameInputMultiplexer.removeProcessor(oldValue.inputProcessor)
+                }
+
+                newValue.resize(Gdx.graphics.width, Gdx.graphics.height)
+                newValue.gameRenderer.shouldApplyViewport.set(true)
+                this.gameInputMultiplexer.addProcessor(newValue.inputProcessor)
+                _container = newValue
+                (currentContainer as Var).set(newValue)
+
+                oldValue?.dispose()
+            }
+            get() = _container ?: error("backingGameContainer is not initialized yet")
+        
+        val currentContainer: ReadOnlyVar<GameContainer> by lazy { Var(current) }
+
+        constructor() : this(gameInputMultiplexer = InputMultiplexer())
+        
+        fun setNewGameContainer(newValue: GameContainer) {
+            this.current = newValue
+        }
+        
+        override fun dispose() {
+            current.dispose()
+        }
+    }
+    
+    private class GameFrameBuffer : Disposable {
+
+        // Note: Game framebuffer is set to stretch scaling, so initial width/height doesn't matter and can be 1
+        val camera: OrthographicCamera = OrthographicCamera().apply {
+            setToOrtho(false, 1f, 1f)
+        }
+        val viewport: ScalingViewport = StretchViewport(1f, 1f, camera)
+        val fbManager: FrameBufferManager = FrameBufferManager(
+            1, FrameBufferManager.BufferSettings(Pixmap.Format.RGB888), scaling = viewport.scaling,
+        )
+
+        override fun dispose() {
+            fbManager.dispose()
+        }
     }
 }
