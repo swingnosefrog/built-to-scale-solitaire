245addda663cdd4c0ecfd8db2ff7d666056d268b
chrislo27 <kinchrislo@gmail.com>
2025-11-02 08:45:00 -0800

    Convert localization file to UTF-8 BOM

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -15,7 +15,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 7, 0, "dev_20251101a")
+    val VERSION: Version = Version(0, 7, 0, "dev_20251102a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
