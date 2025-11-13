71bc529c7ef7435505831c80a708aeee60c58e81
chrislo27 <kinchrislo@gmail.com>
2025-11-13 07:43:53 -0800

    Rename Gameplay settings to Gameplay & UI

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -15,7 +15,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 10, 0, "dev_20251112a")
+    val VERSION: Version = Version(0, 11, 0, "dev_20251113a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
