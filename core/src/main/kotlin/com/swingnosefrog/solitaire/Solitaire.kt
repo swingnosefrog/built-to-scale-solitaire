51c7e22dcd92b55781b9bc2cf69a4c5cb22f32c3
chrislo27 <kinchrislo@gmail.com>
2025-11-22 19:29:34 -0800

    Fix version suffix

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Solitaire.kt
@@ -15,7 +15,7 @@ object Solitaire {
     }
 
     const val TITLE: String = "Built to Scale Solitaire"
-    val VERSION: Version = Version(0, 18, 0, "dev_20251122f")
+    val VERSION: Version = Version(0, 18, 0, "dev_20251122a")
 
     const val DOT_DIRECTORY_NAME: String = ".built-to-scale-solitaire"
     val DOT_DIRECTORY: File by lazy {
