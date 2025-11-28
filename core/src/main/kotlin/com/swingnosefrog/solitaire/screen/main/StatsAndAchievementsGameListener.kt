4b8a881e55d4cf40639822f623e02a882bb8707b
chrislo27 <kinchrislo@gmail.com>
2025-11-27 21:24:46 -0800

    Merge AchievementsLogic into listener, impl second few moves achievement

--- core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/StatsAndAchievementsGameListener.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/screen/main/StatsAndAchievementsGameListener.kt
@@ -6,7 +6,7 @@ import com.swingnosefrog.solitaire.game.logic.CardStack
 import com.swingnosefrog.solitaire.game.logic.CardZone
 import com.swingnosefrog.solitaire.game.logic.GameEventListener
 import com.swingnosefrog.solitaire.game.logic.GameLogic
-import com.swingnosefrog.solitaire.statistics.AchievementsLogic
+import com.swingnosefrog.solitaire.game.logic.GamePlayStats
 import com.swingnosefrog.solitaire.statistics.StatsImpl
 import com.swingnosefrog.solitaire.steamworks.AchievementIds
 import com.swingnosefrog.solitaire.steamworks.SteamStats
@@ -16,7 +16,34 @@ class StatsAndAchievementsGameListener(
     private val stats: StatsImpl,
     private val gameContainer: GameContainer,
 ) : GameEventListener {
-    
+
+    companion object {
+
+        private const val WIN_GAME_FEW_MOVES_THRESHOLD_MOVES: Int = 25
+        private const val WIN_GAME_FEW_MOVES_2_THRESHOLD_MOVES: Int = 20
+        private const val WIN_GAME_FAST_THRESHOLD_SECONDS: Float = 75f
+
+    }
+
+
+    private fun getAchievementsGottenOnGameWin(gamePlayStats: GamePlayStats): List<String> {
+        val movesMade = gamePlayStats.movesMade.get()
+        val timeElapsedSec = gamePlayStats.timeElapsedSec.get()
+
+        return buildList {
+            if (movesMade <= WIN_GAME_FEW_MOVES_THRESHOLD_MOVES) {
+                this += AchievementIds.WIN_GAME_FEW_MOVES
+            }
+            if (movesMade <= WIN_GAME_FEW_MOVES_2_THRESHOLD_MOVES) {
+                this += AchievementIds.WIN_GAME_FEW_MOVES_2
+            }
+
+            if (timeElapsedSec <= WIN_GAME_FAST_THRESHOLD_SECONDS) {
+                this += AchievementIds.WIN_GAME_FAST
+            }
+        }
+    }
+
     private fun handleStatsOnGameWon(gameLogic: GameLogic) {
         stats.gamesWon.increment()
         stats.currentWinStreak.increment()
@@ -28,14 +55,14 @@ class StatsAndAchievementsGameListener(
         steamStats.triggerAchievementProgress(stats)
         steamStats.updateStatCache(stats)
 
-        val achievementsWon = AchievementsLogic.getAchievementsGottenOnGameWin(gameLogic, gameContainer.gamePlayStats)
+        val achievementsWon = getAchievementsGottenOnGameWin(gameContainer.gamePlayStats)
         if (achievementsWon.isNotEmpty()) {
             achievementsWon.forEach { steamStats.markAchievementAsAchieved(it) }
         }
 
         steamStats.persistStats()
     }
-
+    
     override fun onGameWon(gameLogic: GameLogic) {
         handleStatsOnGameWon(gameLogic)
     }
@@ -47,27 +74,15 @@ class StatsAndAchievementsGameListener(
     override fun onDealingStart(gameLogic: GameLogic) {
     }
 
-    override fun onCardStackPickedUp(
-        gameLogic: GameLogic,
-        cardStack: CardStack,
-        fromZone: CardZone,
-    ) {
+    override fun onCardStackPickedUp(gameLogic: GameLogic, cardStack: CardStack, fromZone: CardZone) {
     }
 
-    override fun onCardStackPickupCancelled(
-        gameLogic: GameLogic,
-        cardStack: CardStack,
-        originalZone: CardZone,
-    ) {
+    override fun onCardStackPickupCancelled(gameLogic: GameLogic, cardStack: CardStack, originalZone: CardZone) {
     }
 
-    override fun onCardStackPlacedDown(
-        gameLogic: GameLogic,
-        cardStack: CardStack,
-        toZone: CardZone,
-    ) {
+    override fun onCardStackPlacedDown(gameLogic: GameLogic, cardStack: CardStack, toZone: CardZone) {
         stats.movesMade.increment()
-        
+
         if (gameLogic.isPlayerZoneAndTallStack(toZone)) {
             val steamStats = SteamStats
             val id = AchievementIds.TALL_STACK
@@ -78,30 +93,16 @@ class StatsAndAchievementsGameListener(
         }
     }
 
-    override fun onCardAutoMoved(
-        gameLogic: GameLogic,
-        card: Card,
-        targetZone: CardZone,
-    ) {
+    override fun onCardAutoMoved(gameLogic: GameLogic, card: Card, targetZone: CardZone) {
     }
 
-    override fun onCardPlacedInFoundation(
-        gameLogic: GameLogic,
-        card: Card,
-        foundationZone: CardZone,
-    ) {
+    override fun onCardPlacedInFoundation(gameLogic: GameLogic, card: Card, foundationZone: CardZone) {
     }
 
-    override fun onWidgetSetCompleted(
-        gameLogic: GameLogic,
-        freeCellZone: CardZone,
-    ) {
+    override fun onWidgetSetCompleted(gameLogic: GameLogic, freeCellZone: CardZone) {
     }
 
-    override fun onFoundationZoneCompleted(
-        gameLogic: GameLogic,
-        foundationZone: CardZone,
-    ) {
+    override fun onFoundationZoneCompleted(gameLogic: GameLogic, foundationZone: CardZone) {
     }
 
     override fun onCardsRecollected(gameLogic: GameLogic) {
