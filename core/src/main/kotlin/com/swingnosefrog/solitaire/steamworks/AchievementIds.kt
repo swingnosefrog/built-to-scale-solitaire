b123c9c1865355df9f9ad1514d7e13d2d8dfdfda
chrislo27 <kinchrislo@gmail.com>
2025-11-27 20:59:44 -0800

    Update achievement IDs

--- core/src/main/kotlin/com/swingnosefrog/solitaire/steamworks/AchievementIds.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/steamworks/AchievementIds.kt
@@ -9,6 +9,11 @@ object AchievementIds {
     const val WIN_GAMES_100: String = "win_games_3"
     
     const val WIN_GAME_FEW_MOVES: String = "win_game_few_moves"
+    const val WIN_GAME_FEW_MOVES_2: String = "win_game_few_moves_2"
     const val WIN_GAME_FAST: String = "win_game_fast"
     const val TALL_STACK: String = "tall_stack"
+    const val NO_NUMERICAL_CARDS_IN_FREE_SLOTS: String = "no_numerical_cards_in_free_slots"
+    const val WIDGETS_LAST_TO_ASSEMBLE: String = "widgets_last_to_assemble"
+    const val ASSEMBLE_FULL_SUIT_MANUALLY: String = "assemble_full_suit_manually"
+    const val TRIPLE_SEVENS_FREE_CELL: String = "triple_sevens_free_cell"
 }
