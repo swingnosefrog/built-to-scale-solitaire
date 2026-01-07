a4a755afdfa3d63629c9d2062132b0de264caba0
chrislo27 <kinchrislo@gmail.com>
2026-01-06 20:57:34 -0800

    Add Dutch localization

--- core/src/main/kotlin/com/swingnosefrog/solitaire/Localization.kt
+++ core/src/main/kotlin/com/swingnosefrog/solitaire/Localization.kt
@@ -2,6 +2,7 @@ package com.swingnosefrog.solitaire
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.files.FileHandle
+import paintbox.Paintbox
 import paintbox.binding.ReadOnlyVar
 import paintbox.i18n.LocalePickerBase
 import paintbox.i18n.LocalizationBase
@@ -12,7 +13,32 @@ object SolitaireLocalePicker : LocalePickerBase(
     if (Solitaire.isDevVersion)
         Gdx.files.internal("localization/langs_dev.json")
     else Gdx.files.internal("localization/langs.json")
-)
+) {
+    
+    init {
+        if (Solitaire.isDevVersion) {
+            checkForLocaleOrder()
+        }
+    }
+    
+    private fun checkForLocaleOrder() {
+        val loggingTag = "SolitaireLocalePicker"
+        val allLocales = this.namedLocales
+        
+        val firstNamedLocale = allLocales.first()
+        if (firstNamedLocale.locale.toString() != "") {
+            Paintbox.LOGGER.warn("First locale isn't blank: got \'${firstNamedLocale.locale}\' (${firstNamedLocale.name})", tag = loggingTag)
+        }
+        
+        val tail = allLocales.drop(1)
+        if (tail.size >= 2) {
+            val tailInOrder = tail.sortedBy { it.name }
+            if (tail != tailInOrder) {
+                Paintbox.LOGGER.warn("Non-default ${tail.size} locales aren't in expected name order. Expected to be: ${tailInOrder.map { it.toString() }}", tag = loggingTag)
+            }
+        }
+    }
+}
 
 abstract class SolitaireLocalizationBase(baseHandle: FileHandle) : LocalizationBase(baseHandle, SolitaireLocalePicker)
 
